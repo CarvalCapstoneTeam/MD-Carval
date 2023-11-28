a028e07e232fc2adae7c01cbbad7b653d7305e2d
BillyFranko <billy.franko@mhs.mdp.ac.id>
2023-11-28 12:12:37 +0700

    Animation Progress

--- app/src/main/java/com/dicoding/carvalappandroid/api/APIService.kt
+++ app/src/main/java/com/dicoding/carvalappandroid/api/APIService.kt
@@ -1,4 +1,31 @@
 package com.dicoding.carvalappandroid.api
 
+import com.dicoding.carvalappandroid.response.ArticleResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface APIService {
+//    @FormUrlEncoded
+//    @POST("login")
+//    suspend fun login(
+//        @Field("username") username : String
+//        @Field("password") password : String
+//    ) : LoginResponse
+
+//    @FormUrlEncoded
+//    @POST("register")
+//    suspend fun register(
+//        @Field("email") email : String
+//        @Field("username") username : String
+//        @Field("password") password : String
+//    ) : LoginResponse
+
+    @GET("news")
+    suspend fun getArticles() : ArticleResponse
+
+//    @GET("news/{id}")
+//    suspend fun getDetailArticle(@Path("id") id : String) : DetailResponse
 }
---
+++ app/src/main/java/com/dicoding/carvalappandroid/ui/onboarding/BoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.carvalappandroid.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.carvalappandroid.R
+
+class BoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boarding)
+    }
+}
