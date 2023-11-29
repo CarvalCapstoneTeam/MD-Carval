3f7fc86e00bb75b897b5822375b79f3f4c13ca46
BillyFranko <billy.franko@mhs.mdp.ac.id>
2023-11-29 09:34:00 +0700

    Login page progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("androidx.annotation:annotation:1.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/dicoding/carvalappandroid/ui/login/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package com.dicoding.carvalappandroid.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+
+
+class LoginViewModel() : ViewModel() {
+
+
+}
