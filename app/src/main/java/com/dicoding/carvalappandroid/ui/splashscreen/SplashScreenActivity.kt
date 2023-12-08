263499da923639c6dcaf0e50e25ba2fa5f582a8c
BillyFranko <billy.franko@mhs.mdp.ac.id>
2023-12-08 11:16:42 +0700

    SplashScreenSetup

---
+++ app/src/main/java/com/dicoding/carvalappandroid/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.dicoding.carvalappandroid.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.dicoding.carvalappandroid.R
+import com.dicoding.carvalappandroid.ui.onboarding.BoardingActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, BoardingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
